121d63e2f33e9ae8de19505308bb127dc05cfc76
Marcos Camargo <marcos.camargo@basis.com.br>
2021-09-29 04:11:08 -0300

    Views CRUD de produtos

---
+++ resources/views/Produto/editar.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Editar Produto</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="" title="Voltar"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Error!</strong>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li></li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nome:</strong>
+                    <input type="text" name="name" value="" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Categoria</strong>
+                    <select class="form-control" placeholder="Selecione uma categoria"></select>
+
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Preço</strong>
+                    <input type="number" name="price" class="form-control" placeholder=""
+                        value="">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Salvar</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
